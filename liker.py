07dec2b1ed41801365503d05f171b0af48919085
mixelburg <52622705+mixelburg@users.noreply.github.com>
2020-06-23 09:27:47 +0300

    added unlike

--- liker.py
+++ liker.py
@@ -8,6 +8,7 @@ login_payload = '100#{gli&&er}{"user_name":"<user_name>","password":"<password>"
 auth_payload = '110#{gli&&er}2066##'
 entity_payload = '310#{gli&&er}<id>##'
 like_payload = '710#{gli&&er}{"glit_id":<glit_id>,"user_id":<user_id>,"user_screen_name":"<screen_name>","id":-1}##'
+unlike_payload = '720#{gli&&er}<unlike_id>##'
 
 NUM_LIKES = 1
 GLIT_ID = 0
@@ -15,6 +16,8 @@ USER_ID = 0
 USER_NAME = ""
 USER_SCREEN_NAME = ""
 PASSWORD = ""
+UNLIKE_FLAG = False
+UNLIKE_ID = ""
 
 
 def login(sock):
@@ -22,6 +25,7 @@ def login(sock):
     global USER_SCREEN_NAME
     global entity_payload
     global like_payload
+
 
     payloads = [login_payload, auth_payload, entity_payload]
 
@@ -44,6 +48,7 @@ def login(sock):
 
     print("[+] Login successful")
 
+
 def main(argv):
     global USER_NAME
     global PASSWORD
@@ -51,11 +56,14 @@ def main(argv):
     global GLIT_ID
     global login_payload
     global like_payload
+    global UNLIKE_FLAG
+    global UNLIKE_ID
+    global unlike_payload
 
     try:
-        opts, args = getopt.getopt(argv, "hu:p:n:g:")
+        opts, args = getopt.getopt(argv, "hu:p:n:g:r:")
     except getopt.GetoptError:
-        print("Usage: ping.py -u <user_name> -p <password> -n <num_likes> -g <glit_id>")
+        print("Usage: ping.py -u <user_name> -p <password> -n <num_likes> -g <glit_id> -r <unlike_id> (to unlike) ")
         sys.exit()
 
     for opt, arg in opts:
@@ -74,20 +82,34 @@ def main(argv):
                 sys.exit()
         elif opt == '-g':
             GLIT_ID = arg
+            like_payload = like_payload.replace("<glit_id>", GLIT_ID)
+        elif opt == '-r':
+            UNLIKE_FLAG = True
+            UNLIKE_ID = arg
+            unlike_payload = unlike_payload.replace("<unlike_id>", UNLIKE_ID)
 
     login_payload = login_payload.replace("<user_name>", USER_NAME)
     login_payload = login_payload.replace("<password>", PASSWORD)
-    like_payload = like_payload.replace("<glit_id>", GLIT_ID)
 
     print("[+] Started")
     sock = client()
 
     login(sock)
 
-    for i in range(NUM_LIKES):
-        sock.send(like_payload.encode())
-        data = sock.recv(2048)
-        print("[+] Like added")
+    if not UNLIKE_FLAG:
+        for i in range(NUM_LIKES):
+            sock.send(like_payload.encode())
+            data = sock.recv(2048)
+            print(data.decode())
+    else:
+        for i in range(NUM_LIKES):
+            sock.send(unlike_payload.encode())
+            data = sock.recv(2048)
+            print(data.decode())
+
+            NEW_UNLIKE_ID = str(int(UNLIKE_ID) + 1)
+            unlike_payload = unlike_payload.replace(UNLIKE_ID, NEW_UNLIKE_ID)
+            UNLIKE_ID = NEW_UNLIKE_ID
 
     close(sock)
 
