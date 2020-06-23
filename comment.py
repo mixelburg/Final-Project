aee0eeec13bb931ae4749ac3964435e1abbe89dd
mixelburg <52622705+mixelburg@users.noreply.github.com>
2020-06-23 13:51:31 +0300

    added functionality

--- comment.py
+++ comment.py
@@ -7,17 +7,15 @@ import time
 login_payload = '100#{gli&&er}{"user_name":"<user_name>","password":"<password>","enable_push_notifications":true}##'
 auth_payload = '110#{gli&&er}2066##'
 entity_payload = '310#{gli&&er}<id>##'
-like_payload = '710#{gli&&er}{"glit_id":<glit_id>,"user_id":<user_id>,"user_screen_name":"<screen_name>","id":-1}##'
-unlike_payload = '720#{gli&&er}<unlike_id>##'
+comment_payload = '650#{gli&&er}{"glit_id":<glit_id>,"user_id":<user_id>,"user_screen_name":"<screen_name>","id":-1,"content":"<content>","date":"2020-06-23T06:29:00.751Z"}##'
 
-NUM_LIKES = 1
+
 GLIT_ID = 0
 USER_ID = 0
 USER_NAME = ""
-USER_SCREEN_NAME = ""
+USER_SCREEN_NAME = "default"
 PASSWORD = ""
-UNLIKE_FLAG = False
-UNLIKE_ID = ""
+COMMENT = "default"
 
 
 def login(sock):
@@ -25,7 +23,7 @@ def login(sock):
     global USER_SCREEN_NAME
     global entity_payload
     global like_payload
-
+    global comment_payload
 
     payloads = [login_payload, auth_payload, entity_payload]
 
@@ -40,7 +38,8 @@ def login(sock):
             USER_ID = user_id_info.split(":")[1]
 
             entity_payload = entity_payload.replace("<id>", USER_ID)
-            like_payload = like_payload.replace("<user_id>", USER_ID)
+            comment_payload = comment_payload.replace("<user_id>", USER_ID)
+            comment_payload = comment_payload.replace("<screen_name>", USER_SCREEN_NAME)
 
 
     print("[+] Login successful")
@@ -49,19 +48,16 @@ def login(sock):
 def main(argv):
     global USER_NAME
     global PASSWORD
-    global NUM_LIKES
     global GLIT_ID
     global login_payload
-    global like_payload
-    global UNLIKE_FLAG
-    global UNLIKE_ID
-    global unlike_payload
+    global COMMENT
+    global comment_payload
     global USER_SCREEN_NAME
 
     try:
-        opts, args = getopt.getopt(argv, "hu:p:n:g:r:s:")
+        opts, args = getopt.getopt(argv, "hu:p:g:c:s:")
     except getopt.GetoptError:
-        print("Usage: ping.py -u <user_name> -p <password> -n <num_likes> -g <glit_id> -r <unlike_id> (to unlike) ")
+        print("Usage: ping.py -u <user_name> -p <password> -g <glit_id> -c <comment>")
         sys.exit()
 
     for opt, arg in opts:
@@ -72,46 +68,28 @@ def main(argv):
             USER_NAME = arg
         elif opt == '-p':
             PASSWORD = arg
-        elif opt == '-s':
-            USER_SCREEN_NAME = arg
-            like_payload = like_payload.replace("<screen_name>", USER_SCREEN_NAME)
-        elif opt == '-n':
-            try:
-                NUM_LIKES = int(arg)
-            except:
-                print("NUM_LIKES must be integer")
-                sys.exit()
         elif opt == '-g':
             GLIT_ID = arg
-            like_payload = like_payload.replace("<glit_id>", GLIT_ID)
-        elif opt == '-r':
-            UNLIKE_FLAG = True
-            UNLIKE_ID = arg
-            unlike_payload = unlike_payload.replace("<unlike_id>", UNLIKE_ID)
-
+            comment_payload = comment_payload.replace("<glit_id>", GLIT_ID)
+        elif opt == '-c':
+            COMMENT = arg
+        elif opt == '-s':
+            USER_SCREEN_NAME = arg
+            comment_payload = comment_payload.replace("<screen_name>", USER_SCREEN_NAME)
 
     login_payload = login_payload.replace("<user_name>", USER_NAME)
     login_payload = login_payload.replace("<password>", PASSWORD)
+    comment_payload = comment_payload.replace("<content>", COMMENT)
 
     print("[+] Started")
     sock = client()
 
     login(sock)
 
-    if not UNLIKE_FLAG:
-        for i in range(NUM_LIKES):
-            sock.send(like_payload.encode())
-            data = sock.recv(2048)
-            print(data.decode())
-    else:
-        for i in range(NUM_LIKES):
-            sock.send(unlike_payload.encode())
-            data = sock.recv(2048)
-            print(data.decode())
-
-            NEW_UNLIKE_ID = str(int(UNLIKE_ID) + 1)
-            unlike_payload = unlike_payload.replace(UNLIKE_ID, NEW_UNLIKE_ID)
-            UNLIKE_ID = NEW_UNLIKE_ID
+    print(comment_payload)
+    sock.send(comment_payload.encode())
+    data = sock.recv(2048)
+    print(data.decode())
 
     close(sock)
 
